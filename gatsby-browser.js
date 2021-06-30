731f48b1c1ba5fb09e60932d7a669302501c8f2e
Kip Riecken <kip.riecken@gmail.com>
2021-06-30 13:55:38 -0400

    style: change global font to Darker Grotesque, update navbar link color, increase base font size

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { navigate } from 'gatsby';
+import '@fontsource/darker-grotesque';
 import { MapProvider } from './src/lib/context/mapContext';
 import { GameProvider } from './src/lib/context/gameContext';
 import { AuthProvider } from './src/lib/context/authContext';
