48d17d1042d7217575b6092e9f91a420b3fe9ce8
Ujwal Kandukuri <ujwal.kanduk@gmail.com>
2021-07-03 17:28:17 -0400

    fix: remove carousel

--- src/components/game/ImageCarousel.tsx
+++ src/components/game/ImageCarousel.tsx
@@ -1,7 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Magnifier from 'react-magnifier';
-import Carousel from '@brainhubeu/react-carousel';
-import '@brainhubeu/react-carousel/lib/style.css';
 
 export interface ImageCarouselProps {
   sources: string[];
@@ -9,19 +7,19 @@ export interface ImageCarouselProps {
 
 const ImageCarousel: React.FC<ImageCarouselProps> = ({ sources }) => (
   <div className="carousel">
-    <Carousel plugins={['rtl', 'arrows']}>
-      {sources.map((source, ind) => (
-        <div className="carousel__button">
-          <Magnifier
-            key={ind.toString()}
-            src={source}
-            zoomFactor={1.3}
-            mgWidth={250}
-            mgHeight={250}
-          />
-        </div>
-      ))}
-    </Carousel>
+    {sources.map((source, ind) => (
+      <div className="carousel__button">
+        <Magnifier
+          key={ind.toString()}
+          src={source}
+          zoomFactor={1.3}
+          mgWidth={250}
+          mgHeight={250}
+          height="100%"
+          width="auto"
+        />
+      </div>
+    ))}
   </div>
 );
 
