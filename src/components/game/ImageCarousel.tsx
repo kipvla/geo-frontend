29ee4100640f70b25d042f291e62572a54ffd6fa
Kip Riecken <kip.riecken@gmail.com>
2021-07-05 19:22:28 -0400

    fix: carousel, map size on game

--- src/components/game/ImageCarousel.tsx
+++ src/components/game/ImageCarousel.tsx
@@ -7,20 +7,37 @@ export interface ImageCarouselProps {
 
 const ImageCarousel: React.FC<ImageCarouselProps> = ({ sources }) => (
   <div className="carousel">
-    {sources.map((source, ind) => (
-      <div className="carousel__button">
-        {/* <Magnifier
-          key={ind.toString()}
-          src={source}
-          zoomFactor={1.3}
-          mgWidth={250}
-          mgHeight={250}
-          height="100%"
-          width="auto"
-        /> */}
-        <img key={ind.toString()} src={source} alt={source} />
+    <div className="slider">
+      <div className="slides">
+        {sources.map((source, ind) => (
+          <div
+            id={`slide-${ind + 1}`}
+            style={{ backgroundImage: `url(${source})` }}
+          >
+            {/* <Magnifier
+              key={ind.toString()}
+              src={source}
+              zoomFactor={1.3}
+              mgWidth={250}
+              mgHeight={250}
+              height="100%"
+              width="auto"
+            /> */}
+            {/* <img key={ind.toString()} src={source} alt={source} /> */}
+          </div>
+        ))}
       </div>
-    ))}
+    </div>
+    <div className="thumbnails">
+      {sources.map((source, ind) => (
+        <a
+          href={`#slide-${ind + 1}`}
+          style={{ backgroundImage: `url(${source})` }}
+        >
+          {}
+        </a>
+      ))}
+    </div>
   </div>
 );
 
