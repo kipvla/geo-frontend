b2277db6b82ed61ed14250a7a2a2aa655daaede2
sancar22 <santiagoyacaman@uninorte.edu.co>
2021-07-06 12:57:06 -0500

    feat: linting

--- src/pages/crowdsource.tsx
+++ src/pages/crowdsource.tsx
@@ -1,39 +1,60 @@
 import React, { useState, useRef } from 'react';
 import ImageUploader from 'react-images-upload';
+import axios from 'axios';
+import LoadingBar from 'react-top-loading-bar';
 import Navbar from '../components/presentational/Navbar';
 import formatImages from '../lib/images/base64';
-import apiService from '../services/apiService';
 
 const CrowdSource = () => {
   const inputEl = useRef(null);
   const [error, setError] = useState('');
   const [photosToSend, setPhotosToSend] = useState([]);
-  console.log(inputEl);
+  const [uploadPercentage, setUploadPercentage] = useState(0);
+  const [successMessage, setSuccessMessage] = useState('');
 
   const imageHandler = async (arrayOfImages) => {
     const formattedImages = await formatImages(arrayOfImages);
     setPhotosToSend(formattedImages);
     setError('');
+    setSuccessMessage('');
   };
 
   const submitPhotos = async (e) => {
     e.preventDefault();
-    console.log('PHOTOS ', photosToSend.length);
-    console.log('FILES ', inputEl.current.state.files.length);
-    console.log(photosToSend);
-    const response = await apiService.submitCrowdsourceImages(photosToSend);
-    if (!response.ok) {
-      const body = await response.json();
-      setError(body.msg);
+
+    const options = {
+      onUploadProgress: (progressEvent) => {
+        const { loaded, total } = progressEvent;
+        const percent = Math.floor((loaded * 100) / total);
+        setUploadPercentage(percent);
+      },
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    try {
+      const response = await axios.post(
+        'http://localhost:5000/places/crowdsource',
+        JSON.stringify({ placeImages: photosToSend }),
+        options
+      );
+      setSuccessMessage(response.data.msg);
+    } catch (err) {
+      setError(err.response.data.msg);
     }
     inputEl.current.state.pictures = [];
     inputEl.current.state.files = [];
     setPhotosToSend([]);
-    console.log(inputEl);
   };
 
   return (
     <div className="page__container container">
+      <LoadingBar
+        color="#f11946"
+        height={10}
+        progress={uploadPercentage}
+        onLoaderFinished={() => setUploadPercentage(0)}
+      />
       <Navbar auth />
       <form onSubmit={submitPhotos} className="container">
         <ImageUploader
@@ -47,7 +68,18 @@ const CrowdSource = () => {
           label="Max size: 5mb, Accepted images: jpg"
           withPreview
         />
+        <div
+          style={{
+            marginBottom: '30px',
+            display: uploadPercentage === 0 && 'none',
+          }}
+        >
+          Loading:
+          {`${uploadPercentage}%`}
+        </div>
+
         {error && <p>{error}</p>}
+        {successMessage && <p>{successMessage}</p>}
         <button
           type="submit"
           disabled={photosToSend.length === 0}
