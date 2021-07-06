a444235ede0274f203f30f043d7bbe8a6b4738d3
Kip Riecken <kip.riecken@gmail.com>
2021-07-06 17:29:48 -0400

    fix: add try/catch to multiplayer games hook, update multiplayer navbar icon

--- src/lib/hooks/useFetchMultiplayerGames.ts
+++ src/lib/hooks/useFetchMultiplayerGames.ts
@@ -5,22 +5,28 @@ import apiService from '../../services/apiService';
 export default function useFetchMultiplayerGames() {
   const [multiplayerGames, setMultiplayerGames] = useState([]);
   const fetchGames = async () => {
-    const allGames = await apiService
-      .fetchAllMultiplayerGames()
-      .then((res) => res.json());
-    setMultiplayerGames(allGames.results);
-    console.log(allGames);
-    // TODO played with
+    try {
+      const allGames = await apiService
+        .fetchAllMultiplayerGames()
+        .then((res) => res.json());
+      setMultiplayerGames(allGames.results);
+      console.log(allGames);
+      // TODO played with
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   useEffect(() => {
     fetchGames();
   }, []);
 
-  return multiplayerGames.map((game) => ({
-    ...game,
-    score: game.currentScore,
-    lastPlayed: moment(game.updatedAt).calendar(),
-    multiplayerGameID: game.multiplayerGameID,
-  }));
+  return (
+    multiplayerGames.map((game) => ({
+      ...game,
+      score: game.currentScore,
+      lastPlayed: moment(game.updatedAt).calendar(),
+      multiplayerGameID: game.multiplayerGameID,
+    })) || multiplayerGames
+  );
 }
